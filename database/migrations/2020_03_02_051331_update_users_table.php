57bf86f4ae82e08968c70b26a14bb0f29dc11445
Muhammad Farhan <tapedaily.farhan@gmail.com>
2020-03-03 12:09:36 +0500

    Installed spatie/laravel-permission

--- database/migrations/2020_03_02_051331_update_users_table.php
+++ database/migrations/2020_03_02_051331_update_users_table.php
@@ -14,9 +14,9 @@ class UpdateUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('user_url')->comment('The link to user profile page')->after('password');
+            $table->string('user_url')->comment('The link to user profile page')->default(null)->after('password');
             $table->string('user_activation_key')->nullable()->after('user_url');
-            $table->string('user_status')->comment('1:active, 2:inactive, 3:pending, 4:suspended, 5:dropped')->after('user_activation_key');
+            $table->string('user_status')->comment('1:active, 2:inactive, 3:pending, 4:suspended, 5:dropped')->default('1')->after('user_activation_key');
             $table->string('user_display_name')->nullable()->after('user_status');
             $table->text('user_profile_image')->nullable()->after('user_display_name');
             $table->ipAddress('user_ip')->nullable()->after('user_profile_image');
---
+++ database/seeds/RolesAndPermissionsSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        app()['cache']->forget('spatie.permission.cache');
+
+        for ($i=0; $i<5; $i++) {
+            $role = '';
+            if($i == 0) $role = 'admin';
+            if($i == 1) $role = 'user';
+            if($i == 2) $role = 'editor';
+            if($i == 3) $role = 'contributor';
+            if($i == 4) $role = 'subscriber';
+
+            Role::create(['name' => $role]);
+            $now = Carbon::now()->format('Y-m-d H:i:s');
+            $admin = factory(\App\User::class)->create([
+                'name' => $role,
+                'email' => "mfarhan@{$role}.com",
+                'email_verified_at' => $now,
+                'password' => bcrypt('password'),
+                'user_url' => url()->current().'/'.$role,
+                'user_activation_key' => bcrypt('password'),
+                'user_status' => 1,
+                'user_display_name' => $role,
+                'user_profile_image' => '',
+                'user_ip' => request()->ip(),
+                'created_at' => $now,
+                'updated_at' => $now
+            ]);
+            $admin->assignRole($role);
+        }
+    }
+}
